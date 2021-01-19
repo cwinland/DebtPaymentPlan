a91f831924b569f581109f59b63bda303ee8d326
Christopher Winland <42553203+cwinland@users.noreply.github.com>
2021-01-18 19:47:43 -0500

    Add Tests

--- DebtPlannerTests/DebtPortfolioTests.cs
+++ DebtPlannerTests/DebtPortfolioTests.cs
@@ -8,26 +8,40 @@ namespace DebtPlannerTests
     [TestClass]
     public class DebtPortfolioTests : TestBase
     {
-        
-
-        private DebtPortfolio CreatePortfolio() => new DebtPortfolio { a0, a1, a2, a3, a4, a5, a6, };
+        private DebtPortfolio CreatePortfolio() => new DebtPortfolio { a0, a1, a2, a3, a4, a5, a6, a7, };
 
         [TestMethod]
         public void CreatedPortfolio() => CreatePortfolio().Should().NotBeNull();
 
         [TestMethod]
-        [DataRow(0, 4)]
-        [DataRow(1, 6)]
-        [DataRow(2, 5)]
-        [DataRow(3, 2)]
-        [DataRow(4, 0)]
-        [DataRow(5, 1)]
-        [DataRow(6, 3)]
-        public void OrderedPortfolio(int index, int expectedInfoIndex)
+        [DataRow(0, 7)]
+        [DataRow(5, 0)]
+        [DataRow(6, 1)]
+        [DataRow(4, 2)]
+        [DataRow(7, 3)]
+        [DataRow(1, 4)]
+        [DataRow(3, 5)]
+        [DataRow(2, 6)]
+        public void OrderedPortfolio(int xIndex, int aIndex)
+        {
+            var a = CreatePortfolio();
+            var x = a.OrderBy(info => info.PayoffDays).ToList();
+            x[xIndex].Should().Be(a[aIndex]);
+        }
+
+        [TestMethod]
+        [DataRow(0, 80)]
+        [DataRow(1, 96)]
+        [DataRow(2, 35)]
+        [DataRow(3, 168)]
+        [DataRow(4, 11)]
+        [DataRow(5, 26)]
+        [DataRow(6, 12)]
+        [DataRow(7, 0)]
+        public void NumberOfPayments(int pIndex, int expectedPayments)
         {
             var p = CreatePortfolio();
-            var x = p.OrderBy(info => info.PayoffDays).ToList();
-            x[index].Should().Be(p[expectedInfoIndex]);
+            p[pIndex].GetAmortization().Count.Should().Be(expectedPayments);
         }
 
         [TestMethod]
@@ -42,8 +56,8 @@ namespace DebtPlannerTests
         public void PayoffMonths_Min()
         {
             var p = CreatePortfolio();
-            var x = p.Min(info => info.PayoffMonths);
-            x.Should().Be(a4.PayoffMonths);
+            p.Min(info => info.PayoffMonths).Should().Be(a7.PayoffMonths);
+            p.Where(x => x.Balance > 0).Min(info => info.PayoffMonths).Should().Be(a4.PayoffMonths);
         }
     }
 }
